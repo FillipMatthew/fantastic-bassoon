1c9d171929b7d1073ac600e33165f1b45c29f7ae
FillipMatthew <FillipMatthew@gmail.com>
2024-08-22 23:27:23 +0200

    Fleshing out the parsing of the vehicle data

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
+#include <array>
+#include <cstdint>
 #include <fstream>
 #include <iostream>
+#include <set>
 #include <sstream>
 
 using namespace std;
+
+struct SVehicleData
+{
+	int32_t id;
+	std::string registration;
+	float latitude;
+	float longitude;
+	uint64_t recordedTimeUTC;
+};
+
+bool LoadDB(const std::string & fileName);
+size_t ReadEntry(char * buffer, size_t bufferLen, SVehicleData & vehicle);
 
 int main(int argc, char ** argv)
 {
@@ -33,13 +48,8 @@ int main(int argc, char ** argv)
 
 	cout << "Using block size of " << blockSize << endl;
 
-	ifstream dbFile;
-	dbFile.open(argv[1], ios::binary);
-	if (!dbFile.is_open())
-	{
-		cout << "Could not open the db file: " << argv[1];
+	if (!LoadDB(argv[1]))
 		return -1;
-	}
 
 	ifstream searchParamFile;
 	searchParamFile.open(argv[2]);
@@ -59,5 +69,53 @@ int main(int argc, char ** argv)
 	}
 
 	cout << "Finished!" << endl;
+	return 0;
+}
+
+bool LoadDB(const std::string & fileName)
+{
+	ifstream dbFile;
+	dbFile.open(fileName, ios::binary);
+	if (!dbFile.is_open())
+	{
+		cout << "Could not open the db file: " << fileName;
+		return false;
+	}
+
+	static const size_t BUFFER_SIZE = 1024;
+	std::array<char, BUFFER_SIZE> buffer;
+
+	std::set<int32_t> vehicleIds;
+
+	size_t bufferLen = 0;
+	while (!dbFile.eof() && !dbFile.fail())
+	{
+		bufferLen += dbFile.readsome(buffer.data(), BUFFER_SIZE - bufferLen);
+
+		size_t processed = 0;
+		SVehicleData vehicle;
+		size_t read = 0;
+		while ((read = ReadEntry(buffer.data() + processed, bufferLen - processed, vehicle)) > 0)
+		{
+			if (vehicleIds.count(vehicle.id))
+				cout << "Multiple entries for: " << vehicle.id << endl;
+
+			vehicleIds.insert(vehicle.id);
+
+			processed += read;
+		}
+
+		if (processed < bufferLen)
+		{
+			memmove(buffer.data(), buffer.data() + processed, bufferLen - processed);
+			bufferLen -= processed;
+		}
+	}
+
+	return true;
+}
+
+size_t ReadEntry(char * buffer, size_t bufferLen, SVehicleData & vehicle)
+{
 	return 0;
 }
